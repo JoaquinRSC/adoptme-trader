6683cd9c233f74a01c9d235ae16b33449d10fe01
Joaquin Rossi <joaquinrossi55@gmail.com>
2026-04-26 19:35:33 -0300

    fix: switch router to history mode so server-side auth redirects work

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createMemoryHistory, createWebHashHistory } from 'vue-router'
+import { createRouter, createMemoryHistory, createWebHistory } from 'vue-router'
 import routes from './routes'
 
 export default createRouter({
   scrollBehavior: () => ({ left: 0, top: 0 }),
   routes,
-  history: process.env.SERVER ? createMemoryHistory() : createWebHashHistory(),
+  history: process.env.SERVER ? createMemoryHistory() : createWebHistory(),
 })
